d244973bce35c220e8dd8ba6d8aaebcc10258ad1
Brice Sarrazin <1295059+b-sarrazin@users.noreply.github.com>
2023-03-04 20:39:55 +0100

    Create OSDCloud.ps1

---
+++ OSDCloud.ps1
@@ -0,0 +1,3 @@
+Install-Module OSD -Force
+Import-Module OSD
+Start-OSDCloud -Firmware -Screenshot -SkipAutopilot -SkipODT -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Enterprise -OSLanguage fr-fr -OSLicense Retail
