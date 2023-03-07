dd7d725342c9b3f33f2246e8636186d0988fc0d8
Brice Sarrazin <1295059+b-sarrazin@users.noreply.github.com>
2023-03-07 18:20:49 +0100

    Delete OSDCloud.ps1

--- OSDCloud.ps1
+++
@@ -1,3 +0,0 @@
-Install-Module OSD -Force
-Import-Module OSD
-Start-OSDCloud -Firmware -Screenshot -SkipAutopilot -SkipODT -OSVersion 'Windows 10' -OSBuild 22H2 -OSEdition Enterprise -OSLanguage fr-fr -OSLicense Retail
